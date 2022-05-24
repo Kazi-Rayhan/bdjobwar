7ab591c267c12277c8933e7d4247e2274cea4b47
Ahmed Tamim <ahmedtamim19050@gmail.com>
2022-05-25 02:25:42 +0600

    home page complete

--- resources/views/frontEnd/test.blade.php
+++ resources/views/frontEnd/test.blade.php
@@ -109,8 +109,8 @@
     <h1>Live Section</h1>
   </div>
         <div class="container">
-           <div class="d-flex justify-content-around">
-            <div class="live-exams">
+           <div class="row">
+            <div class="col-md-6">
                 <h6 class="mt-5 fw-bold live-exam-heading mb-4">
                 <i class="far fa-file-alt fs-3 text-muted"></i> <span class="text-success">Live testing in progress</span>
                 </h6>
@@ -133,25 +133,25 @@
                     <a class="test-btn" href="">Give the test</a>
                 </div>
             </div>
-            <div class="live-exams">
+            <div class="col-md-4 ms-md-5 ps-md-5">
                 <h6 class="mt-5 fw-bold live-exam-heading mb-4">
                 <i class="fas fa-file-invoice fs-3 text-muted"></i> <span class="text-success">Upcoming tests</span>
                 </h6>
-                <div class="card mb-3" style="width: 25rem;">
+                <div class="card mb-3 col-12" style="">
                   <div class="card-body">
                     <h6 class="up-exam-title"><a href="">44th BCS Prelim Final Model Test</a></h6>
                     <h6 class="up-exam-subtitle mb-2 text-muted my-3"><a class="" href="">Model test-2</a></h6>
                     <p class="live-exam-date"><span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM </span>| To <span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM</span></p>
                   </div>
                 </div>
-                <div class="card mb-3" style="width: 25rem;">
+                <div class="card mb-3 col-12" >
                   <div class="card-body">
                     <h6 class="up-exam-title"><a href="">44th BCS Prelim Final Model Test</a></h6>
                     <h6 class="up-exam-subtitle mb-2 text-muted my-3"><a class="" href="">Model test-2</a></h6>
                     <p class="live-exam-date"><span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM </span>| To <span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM</span></p>
                   </div>
                 </div>
-                <div class="card mb-3" style="width: 25rem;">
+                <div class="card mb-3 col-12" >
                   <div class="card-body">
                     <h6 class="up-exam-title"><a href="">44th BCS Prelim Final Model Test</a></h6>
                     <h6 class="up-exam-subtitle mb-2 text-muted my-3"><a class="" href="">Model test-2</a></h6>
@@ -161,6 +161,59 @@
    
             </div>
           </div>
+          <div class="row">
+            <div class="col-md-6 mb-4">
+              <h6 class="mt-5 fw-bold live-exam-heading mb-4">
+              <i class="fas fa-poll-h fs-3 text-muted"></i> <span class="text-success">Latest results</span>
+                </h6>
+              <table class="table table-striped">
+                  <thead class="text-muted">
+                    <tr>
+                      <th scope="col">Exam name</th>
+                      <th scope="col">Subjects</th>
+                      <th scope="col">Result</th>
+                    </tr>
+                  </thead>
+                  <tbody class="text-muted">
+                    <tr>
+                      <td>Model test-1</td>
+                      <td>BCS Prelim all subjects</td>
+                      <td><a href="" class="btn-danger">See</a></td>
+                    </tr>
+                    <tr>
+                      <td>Model test-2</td>
+                      <td>BCS Prelim all subjects</td>
+                      <td><a href="" class="btn-danger">See</a></td>
+                    </tr>
+                    <tr>
+                      <td>Model test-3</td>
+                      <td>BCS Prelim all subjects</td>
+                      <td><a href="" class="btn-danger">See</a></td>
+                    </tr>
+                  </tbody>
+              </table>
+            </div>
+            <div class="col-md-4 ms-md-5 ps-md-5 mb-4">
+                <h6 class="mt-5 fw-bold live-exam-heading mb-4">
+                <i class="fas fa-file-invoice fs-3 text-muted"></i> <span class="text-success">Notice board</span>
+                </h6>
+                <div class="card mb-3 col-md-12" style="">
+                  <div class="card-body">
+                    <h6 class="up-exam-title"><a href="">44th BCS Prelim Final Model Test</a></h6>
+                    <p class="live-exam-date pt-2"><span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM </span></p>
+                  </div>
+                </div>
+                <div class="card mb-3 col-md-12" style="">
+                  <div class="card-body">
+                    <h6 class="up-exam-title"><a href="">44th BCS Prelim Final Model Test</a></h6>
+                    <p class="live-exam-date pt-2"><span><i class="far fa-calendar-alt"></i> 22 May, 2022 10:00 AM </span></p>
+                  </div>
+                </div>
+     
+
+            </div>
+          </div>
+          
       </div>
 </section>
 <!-- Live section end -->
@@ -282,7 +335,162 @@
     </div>
    </div>
 </section>
-<!-- Package section end -->
+<!-- Courses section start -->
+<section class="live-section">
+  <div class="live-section-title" style="background-image: url({{asset('frontEnd-assets/img/Blog.png')}})">
+    <h1>Our Courses</h1>
+  </div>
+   <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Bcs-prep.png')}}" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Subject-Care.png')}}" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Job-Solution.png')}}" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Bcs-prep.png')}}" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Subject-Care.png')}}" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 mb-5 ">
+        <a href="">
+        <img class="img-fluid" src="{{asset('frontEnd-assets/img/Job-Solution.png')}}" alt="">
+        </a>
+      </div>
+   </div>
+</section>
+<!-- Course section end -->
+<!--  -->
+<section class="mt-4">
+  <div class="bg-danger">
+    <div class="d-flex justify-content-around flex-wrap pb-4">
+      <div class="subscribers mt-3">
+        <i class="fas fa-users text-white d-block d-flex justify-content-center fs-1"></i>
+        <p class="text-white mt-2">Subscribers</p>
+        <h5 class="text-white fw-bold text-center">540</h5>
+      </div>
+      <div class="subscribers mt-3">
+        <i class="far fa-file-alt text-white d-block d-flex justify-content-center fs-1"></i>
+        <p class="text-white mt-2">Model tests</p>
+        <h5 class="text-white fw-bold text-center">320</h5>
+      </div>
+      <div class="subscribers mt-3">
+        <i class="far fa-question-circle text-white d-block d-flex justify-content-center fs-1"></i>
+        <p class="text-white mt-2">Question number</p>
+        <h5 class="text-white fw-bold text-center">4540</h5>
+      </div>
+      <div class="subscribers mt-3">
+        <i class="fas fa-chalkboard text-white d-block d-flex justify-content-center fs-1"></i>
+        <p class="text-white mt-2">Course number</p>
+        <h5 class="text-white fw-bold text-center">14</h5>
+      </div>
+
+  
+    </div>
+  </div>
+</section>
+<!--  -->
+<!-- footer section start-->
+<footer>
+    <div class="">
+      <div class="row">
+          <div class="col-md-3 mt-3 footer-info">
+                <div class="brand">
+                  <a class="navbar-brand" href="#">
+                        <img src="{{asset('frontEnd-assets/img/logo.png')}}" alt="">
+                  </a>
+                  <p class="location">Barishal,  Bangladesh</p>
+                  <div class="contact">
+                  <p class=""><i class="fas fa-phone-alt text-danger"></i>+88 01*******</p>
+                  <p class=""><i class="far fa-envelope text-danger"></i>test@test.com</p>
+                  </div>
+                </div>
+                    
+            </div>
+            <div class="col-md-8 bg-footer ">
+                <div class="d-flex flex-wrap justify-content-around">
+                    <div class="populer-course">
+                            <h5 class="mt-3">Populer Courses</h5>
+                            <a href="">
+                              <p>Daily quiz-01</p>
+                            </a>
+                            <a href="">
+                              <p>Daily quiz-02</p>
+                            </a>
+                            <a href="">
+                              <p>Daily quiz-03</p>
+                            </a>
+                    </div>
+                    <div class="populer-course">
+                            <h5 class="mt-3">Populer Package</h5>
+                            <a href="">
+                              <p>Monthly</p>
+                            </a>
+                            <a href="">
+                              <p>Three monts</p>
+                            </a>
+                            <a href="">
+                              <p>Six months</p>
+                            </a>
+                            <a href="">
+                              <p>Yearly</p>
+                            </a>
+                    </div>
+                    <div class="populer-course">
+                            <h5 class="mt-3">Job Guidline</h5>
+                            <a href="">
+                              <p>BCS</p>
+                            </a>
+                            <a href="">
+                              <p>Bank</p>
+                            </a>
+                            <a href="">
+                              <p>Focus Writing</p>
+                            </a>
+                            <a href="">
+                              <p>Yearly</p>
+                            </a>
+                    </div>
+                </div>
+            </div>
+      </div>
+      <div class="d-flex justify-content-around flex-wrap mid-nav">
+       <div class="social">
+        <p class="text-danger mt-2">Stay with us</p>
+          <p><a class="text-decoration-none  text-primary" href=""><i class="fab fa-facebook-f fs-3 me-2"></i></a> <a class="text-decoration-none text-danger" href=""><i class="fab fa-youtube fs-3"></i></a></p>
+        </div>
+   
+      <div class="payment-method">
+        <img class="" src="{{asset('frontEnd-assets/img/ssl.png')}}" alt="">
+      </div>
+      </div>
+      <div class="row">
+          <div class="col-md-3 mt-3 footer-info">
+            <p class="copy-write">Copyright © All Rights Reserved by 2022-2024 Quiz Carnival</p>
+                    
+            </div>
+            <div class="col-md-8 bg-footer dev-team">
+             <p class="text-white mt-2 text-center">Developed By <a class="text-decoration-none text-danger" href="">Wizerd</a></p>
+            </div>
+      </div>
+    </div>
+</footer>
+<!-- footer section end -->
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
