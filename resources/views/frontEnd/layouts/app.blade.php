fa47d1bbbf7cfa09c1140ded938c2e9f587914bb
Kazi Rayhan <thisiskazi@gmail.com>
2022-06-18 13:24:02 +0600

    design changes

--- resources/views/frontEnd/layouts/app.blade.php
+++ resources/views/frontEnd/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'BD Job War') }}</title>
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
