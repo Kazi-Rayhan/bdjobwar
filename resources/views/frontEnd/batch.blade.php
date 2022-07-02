8bbaf2cce3d8b3980f7b6673d34a18d0ee167695
Kazi Rayhan <thisiskazi@gmail.com>
2022-07-03 02:09:09 +0600

    asdas

--- resources/views/frontEnd/batch.blade.php
+++ resources/views/frontEnd/batch.blade.php
@@ -97,7 +97,7 @@ $numto = new NumberToBangla();
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="syllabus">
-       {{$exam->syllabus}}
+    
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
