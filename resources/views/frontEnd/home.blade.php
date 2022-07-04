a00a9af67f78832b8478ef38e32f15a0924d5e12
Kazi Rayhan <thisiskazi@gmail.com>
2022-07-04 12:37:23 +0600

    new changes

--- resources/views/frontEnd/home.blade.php
+++ resources/views/frontEnd/home.blade.php
@@ -196,7 +196,10 @@ $numto = new NumberToBangla();
 
       <div class="col-md-4">
         <div class="card border-success shadow package-hover">
-          <img src="{{Voyager::image($course->thumbnail)}}" height="300px" style="object-fit:contain" class="card-img-top" alt="...">
+          <div class="bg-success rounded">
+            <img src="{{Voyager::image($course->thumbnail)}}" height="300px" style="object-fit:contain" class="rounded card-img-top" alt="...">
+
+          </div>
           <div class="card-body">
             <h3 class="card-title">{{$course->title}}</h3>
             <div style="height:2px;width:100px" class="bg-danger"></div>
