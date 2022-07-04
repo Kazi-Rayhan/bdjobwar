82f741bafbabe91b2986f0a8d677570ae14d48c9
Kazi Rayhan <thisiskazi@gmail.com>
2022-07-04 12:03:15 +0600

    new changes

--- resources/views/frontEnd/layouts/app.blade.php
+++ resources/views/frontEnd/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'BD Job War') }}</title>
+  <title>BD Job War</title>
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -91,7 +91,7 @@
   <!-- mid nav end -->
   <!-- Main Nav start -->
  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" style="background-color:#161E31 !important">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" style="background-color:#ff9027 !important">
     <div class="container">
       <a class="navbar-brand" href="#">
         <img src="{{asset('frontEnd-assets/img/logo.png')}}" alt="" width="100" height="50" style="object-fit:cover">
