deb644c5f2ceb6dceb28f2b48b52575979ed84fd
Kazi Rayhan <thisiskazi@gmail.com>
2022-07-14 17:43:34 +0600

    asdasd

--- resources/views/frontEnd/batch-details.blade.php
+++ resources/views/frontEnd/batch-details.blade.php
@@ -66,22 +66,18 @@
     $(document).ready(function() {
         $(".owl-carousel").owlCarousel({
     loop:true,
+    nav:false,
+    autoplay:true,
     margin:10,
     responsiveClass:true,
     responsive:{
         0:{
             items:1,
-            nav:true
+
         },
         600:{
             items:3,
-            nav:false
         },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
     }
 });
     });
