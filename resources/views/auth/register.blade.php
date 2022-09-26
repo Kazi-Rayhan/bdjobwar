3143cbba705d948f8077a278f53562deed71ead1
Kazi Rayhan <thisiskazi@gmail.com>
2022-09-26 10:49:57 +0600

    asdasd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,109 +1,125 @@
 @extends('frontEnd.layouts.app')
 
 @section('content')
+    <!-- bratcam area  end-->
+    <div class="container my-5 py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <p>
+                    আপনি নতুন ইউজার হয়ে থাকলে রেজিস্ট্রেশন ফর্ম পূরণ করুন। পূর্বে রেজিস্ট্রেশন করে থাকলে পুনরায় রেজিস্ট্রেশন
+                    করার প্রয়োজন নেই। রেজিস্ট্রেশনকৃত মোবাইল নম্বর ও পাসওয়ার্ড দিয়ে লগইন করলেই হবে। লগইন করতে এখানে ক্লিক
+                    করুন।
+                </p>
+                <div class="card shadow-lg">
+                    <div class="card-header py-3 bg-success text-white fs-5">{{ __('রেজিস্ট্রেশন ফরম') }}</div>
 
-<!-- bratcam area  end-->
-<div class="container my-5 py-5">
-    <div class="row justify-content-center">
-        <div class="col-md-5">
-            <div class="card shadow-lg">
-                <div class="card-header py-3 bg-success text-white fs-5">{{ __('রেজিস্ট্রেশন ফরম') }}</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                            <div class="row mb-4 px-4 pt-4">
 
-                        <div class="row mb-4 px-4 pt-4">
+                                <div class="col-md-12">
+                                    <input id="name" placeholder="আপনার নাম" type="text"
+                                        class="border border-success form-control py-2 @error('name') is-invalid @enderror"
+                                        name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                            <div class="col-md-12">
-                                <input id="name" placeholder="আপনার নাম" type="text" class="border border-success form-control py-2 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-4 px-4">
+                            <div class="row mb-4 px-4">
 
-                            <div class="col-md-12">
-                                <input id="phone" type="phone" placeholder="মোবাইল নম্বর" class=" border border-success form-control py-2 @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
-                                @error('phone')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <div class="col-md-12">
+                                    <input id="phone" type="phone" placeholder="মোবাইল নম্বর"
+                                        class=" border border-success form-control py-2 @error('phone') is-invalid @enderror"
+                                        name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+                                    @error('phone')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <!-- <div class="col-md-3 mt-2">
+                                    <a href="#" class="btn-danger" id="otp-btn" style="cursor:pointer">OTP send</a>
+                                </div>  -->
                             </div>
-                            <!-- <div class="col-md-3 mt-2">
-                                <a href="#" class="btn-danger" id="otp-btn" style="cursor:pointer">OTP send</a>
-                            </div>  -->
-                        </div>
 
-                      
- 
-                        <div class="row mb-4 px-4">
 
-                            <div class="col-md-12">
-                                <input id="password" type="password" placeholder="পাসওয়ার্ড" class=" border border-success form-control py-2 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                <button type="button" id="hidePassword" style="" onclick="myFunction()" class="field-icon"><i class=" text-secondary fas fa-eye"></i></button>
-                                <div id="emailHelp" class="form-text">আপনার পছন্দমত পাসওয়ার্ড সেট করুন ।</div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="row mb-4 px-4">
+
+                                <div class="col-md-12">
+                                    <input id="password" type="password" placeholder="পাসওয়ার্ড"
+                                        class=" border border-success form-control py-2 @error('password') is-invalid @enderror"
+                                        name="password" required autocomplete="new-password">
+                                    <button type="button" id="hidePassword" style="" onclick="myFunction()"
+                                        class="field-icon"><i class=" text-secondary fas fa-eye"></i></button>
+                                    <div id="emailHelp" class="form-text">আপনার পছন্দমত পাসওয়ার্ড সেট করুন ।</div>
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-4 px-4">
+                            <div class="row mb-4 px-4">
 
-                            <div class="col-md-12">
-                                <input id="password-confirm" type="password" placeholder="কনফার্ম পাসওয়ার্ড" class=" border border-success form-control py-2" name="password_confirmation" required autocomplete="new-password">
-                                <button type="button" id="hideConfirmPassword" style="" onclick="myFunction2()" class="field-icon"><i class=" text-secondary fas fa-eye"></i></button>
-                                <div id="emailHelp" class="form-text">উপরের পাসওয়ার্ডটি পুনরায় দিন ।</div>
+                                <div class="col-md-12">
+                                    <input id="password-confirm" type="password" placeholder="কনফার্ম পাসওয়ার্ড"
+                                        class=" border border-success form-control py-2" name="password_confirmation"
+                                        required autocomplete="new-password">
+                                    <button type="button" id="hideConfirmPassword" style="" onclick="myFunction2()"
+                                        class="field-icon"><i class=" text-secondary fas fa-eye"></i></button>
+                                    <div id="emailHelp" class="form-text">উপরের পাসওয়ার্ডটি পুনরায় দিন ।</div>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="ms-4">
-                                <button type="submit" class="btn btn-success">
-                                ফ্রি রেজিস্ট্রেশন করুন 
-                                </button>
+                            <div class="row mb-0">
+                                <div class="ms-4">
+                                    <button type="submit" class="btn btn-success">
+                                        ফ্রি রেজিস্ট্রেশন করুন
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('js')
-<script>
-    function myFunction() {
-  var x = document.getElementById("password");
-  
-  if (x.type === "password") {
-    x.type = "text";
-    document.getElementById('hidePassword').innerHTML = '<i class=" text-secondary fas fa-eye-slash"></i>'
-  } else {
-    x.type = "password";
-    document.getElementById('hidePassword').innerHTML = '<i class=" text-secondary fas fa-eye"></i>'
-  }
-} 
-    function myFunction2() {
-  var x = document.getElementById("password-confirm");
-  
-  if (x.type === "password") {
-    x.type = "text";
-    document.getElementById('hideConfirmPassword').innerHTML = '<i class=" text-secondary fas fa-eye-slash"></i>'
-  } else {
-    x.type = "password";
-    document.getElementById('hideConfirmPassword').innerHTML = '<i class=" text-secondary fas fa-eye"></i>'
-  }
-} 
-</script>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("password");
+
+            if (x.type === "password") {
+                x.type = "text";
+                document.getElementById('hidePassword').innerHTML = '<i class=" text-secondary fas fa-eye-slash"></i>'
+            } else {
+                x.type = "password";
+                document.getElementById('hidePassword').innerHTML = '<i class=" text-secondary fas fa-eye"></i>'
+            }
+        }
+
+        function myFunction2() {
+            var x = document.getElementById("password-confirm");
+
+            if (x.type === "password") {
+                x.type = "text";
+                document.getElementById('hideConfirmPassword').innerHTML =
+                    '<i class=" text-secondary fas fa-eye-slash"></i>'
+            } else {
+                x.type = "password";
+                document.getElementById('hideConfirmPassword').innerHTML = '<i class=" text-secondary fas fa-eye"></i>'
+            }
+        }
+    </script>
 @endsection
