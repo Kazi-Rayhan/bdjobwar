4df5a29131e5f673a82647cd02b2a36405f3dd8d
Kazi Rayhan <thisiskazi@gmail.com>
2023-05-22 17:53:45 +0600

    done

--- app/Http/Middleware/IsBanFromBatch.php
+++ app/Http/Middleware/IsBanFromBatch.php
@@ -17,8 +17,8 @@ class IsBanFromBatch
     public function handle(Request $request, Closure $next)
     {
 
-        if (auth()->check() && @auth()->user()->bought($request->batch->id) && @!$request->batch->studentStatus(auth()->user())) {
-            return abort(403,'আপনি ব্যচটি থেকে বহিষ্কার হয়েছেন । অনুগ্রহ করে এডমিনের সাথে যোগাযোগ করুন ।');
+        if (auth()->check() && @auth()->user()->bought($request->batch->id) && !auth()->user()->information->is_paid && @!$request->batch->studentStatus(auth()->user())) {
+            return abort(403, 'আপনি ব্যচটি থেকে বহিষ্কার হয়েছেন । অনুগ্রহ করে এডমিনের সাথে যোগাযোগ করুন ।');
         } else {
             return $next($request);
         }
